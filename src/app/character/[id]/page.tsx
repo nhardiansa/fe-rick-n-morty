839167543cca87e124a31fa95160ef5cbae1df84
Nabil Hardiansa <nhardiansa@gmail.com>
2025-12-09 18:20:05 +0800

    feat: optimize character fetching in generateStaticParams function

--- src/app/character/[id]/page.tsx
+++ src/app/character/[id]/page.tsx
@@ -5,9 +5,22 @@ export async function generateStaticParams() {
   const res = await fetch('https://rickandmortyapi.com/api/character');
   const data = await res.json();
 
+  const pages = []
+
+  // generate all url with page number
+  for (let i = 1; i <= data.info.pages; i++) {
+    pages.push(`https://rickandmortyapi.com/api/character?page=${i}`)
+  }
+
+  // do Promise All to fetch All characters
+  const response = await Promise.all(pages.map((page) => fetch(page).then((res) => res.json().then((data) => data.results))))
+  const allCharacters = response.flat()
+  const allCharactersId = allCharacters.map((character) => character.id)
+
+
   // Ambil semua ID karakter yang ada
-  return data.results.map((character: { id: string }) => ({
-    id: character.id.toString(),
+  return allCharactersId.map((id) => ({
+    id: id.toString(),
   }));
 }
 
